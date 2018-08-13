9b76c29f0a8a5687c56b276b61b934fa184661f3
Steven Ang <spikegreen2@gmail.com>
2018-08-13 19:00:55 +0800

    Add rspec test for Tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,8 @@
 class Tweet < ApplicationRecord
   belongs_to :user
 
+  validates :content, :presence => true, length: {:minimum => 1, :maximum => 240}, :allow_blank => false
+
   enum tweet_type: {pending: 0, scheduled: 1, sent: 2, failed: 3, archived: 4}
 
   scope :scheduled, -> { where("tweet_type = ?", 1) }
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -2,8 +2,43 @@ require "rails_helper"
 
 RSpec.describe Tweet, type: :model do
 
-  context "validation" do
+  before(:each) do
+    @user = User.create!(name: "Steven", nickname: "ProfessorSpike", email: "steven@gmail.com", password: "hellothere")
+    @tweet = Tweet.create!(content: "I'm a very happy person!", user_id: @user.id)
+  end
 
+  context "validation" do
+    it "should be created with valid inputs" do
+      expect(@tweet).to be_valid
+    end
+
+    it "should not allow content to be created if the length is over 240 characters" do
+      @tweet.content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae iure consequuntur asperiores molestiae error maiores recusandae dolores nesciunt distinctio perspiciatis quibusdam id nulla impedit officiis, quia eum porro? Totam cupiditate tempora impedit reprehenderit dolores mollitia quae et velit delectus dolorum eius consectetur quod suscipit, maxime distinctio quis accusantium repellendus praesentium sit. Nam, deleniti delectus nemo itaque dicta temporibus a repellat maiores fugit porro voluptatum ipsa molestias aperiam ullam veritatis officia!"
+      expect(@tweet).to_not be_valid
+    end
+
+    it "should not allow content to be blank" do
+      @tweet.content = ""
+      expect(@tweet).to_not be_valid
+    end
+
+    it "should not allow content to be nil" do
+      @tweet.content = nil
+      expect(@tweet).to_not be_valid
+    end
+
+  end
+
+  describe "association" do
+    it "should have a user" do
+      Tweet.reflect_on_association(:user).macro.should   eq(:belongs_to)
+    end
+  end
+
+  describe "creation" do
+    it "should have one item" do
+      expect(Tweet.all.count).to eq(1)
+    end
   end
 
 end
